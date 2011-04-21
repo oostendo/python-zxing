fb6329bcd4833046ebe908e079f32b9b98c737b5
Nate Oostendorp <nate@oostendorp.net>
2011-04-21 02:48:19 +0000

    initial commit

---
+++ README.markdown
@@ -0,0 +1,47 @@
+python-zxing a quick and dirty wrapper fo the ZXing barcode library
+
+Instructions for use
+
+    wget http://code.google.com/p/zxing/downloads/detail?name=ZXing-1.6.zip
+    cd zxing-1.6 
+    ant -f core/build.xml
+    ant -f javase2/build.xml 
+    git clone git://github.com/oostendo/python-zxing.git
+    cd python-zxing
+    nosetest tests.py
+
+The library consists of two classes, BarCodeReader and BarCode.  BarCode parses
+the output from ZXing's CommandLineRunner into a BarCode object which includes:
+
+    b = zxing.BarCode("""
+    file:default.png (format: FAKE_DATA, type: TEXT):
+    Raw result:
+    foo-bar|the bar of foo
+    Parsed result:
+    foo-bar 
+    the bar of foo
+    Also, there were 4 result points:
+      Point 0: (24.0,18.0)
+      Point 1: (21.0,196.0)
+      Point 2: (201.0,198.0)
+      Point 3: (205.23952,21.0)
+    """)
+    
+    print b.datatype #FAKE_DATA
+    print b.raw #foo-bar|the bar of foo
+    print b.data #foo-bar\nthe bar of foo
+    print b.points #[(24.0, 18.0) ... ]
+
+Initializing the barcode reader, you have to tell it where to find the ZXing
+core modules.  It is the single parameter, which defaults to the parent 
+directory.
+
+reader = zxing.BarCodeReader("/var/opt/zxing")
+
+barcode = reader.decode("/tmp/image.jpg")
+(barcode1, barcode2) = reader.decode(["/tmp/1.png", "/tmp/2.png"])
+code_list = reader.decode("/tmp/barcodes", True)
+
+decode() takes an image path, directory, or list of images and has an optional parameter to use the "try_harder" option.  If no barcode is found, it returns None objects. 
+
+Installation is manual. 
---
+++ zxing.py
@@ -0,0 +1,105 @@
+########################################################################
+#
+#  zxing.py -- a quick and dirty wrapper for zxing for python
+#
+#  this allows you to send images and get back data from the ZXing
+#  library:  http://code.google.com/p/zxing/
+#
+#  by default, it will expect to be run from the zxing source code directory
+#  otherwise you must specify the location as a parameter to the constructor
+#
+
+import subprocess, re
+
+class CodeReader():
+  location = ""
+  command = "java"
+  libs = ["javase/javase.jar", "core/core.jar"]
+  args = ["-cp", "LIBS", "com.google.zxing.client.j2se.CommandLineRunner"]
+
+  def __init__(self, loc=".."):
+    self.location = loc	
+
+  def decode(self, files, try_harder = False):
+    cmd = [self.command]
+    cmd += self.args[:] #copy arg values
+    if (try_harder):
+      cmd.append("--try_harder")
+
+    libraries = [self.location + "/" + l for l in self.libs]
+
+    cmd = [ c if c != "LIBS" else ":".join(libraries) for c in cmd ]
+
+    #send one file, or multiple files in a list
+    SINGLE_FILE = False
+    if type(files) != type(list()):
+      cmd.append(files)
+      SINGLE_FILE = True
+    else:
+      cmd += files
+
+    (stdout, stderr) = subprocess.Popen(cmd, stdout=subprocess.PIPE).communicate()
+    codes = []
+    file_results = stdout.split("\nfile:")
+    for result in file_results:
+      print result
+      lines = stdout.split("\n")
+      if (re.search("No barcode found", lines[0])):
+        codes.append(None)
+        continue
+
+      codes.append(BarCode(result))
+
+    if (SINGLE_FILE):
+      return codes[0]
+    else:
+      return codes
+      	
+#this is the barcode class which has 
+class BarCode:
+  codetype = ""
+  points = []
+  data = ""
+  raw = ""
+
+  def __init__(self, zxing_output):
+    lines = zxing_output.split("\n")
+    raw_block = False
+    parsed_block = False
+    point_block = False
+
+    for l in lines:
+      m = re.search("format:\s([^,]+)", l)
+      if (not raw_block and not parsed_block and not point_block and m):
+        self.codetype = m.group(1)
+        continue
+
+      if (not raw_block and not parsed_block and not point_block and l == "Raw result:"):
+        raw_block = True
+        continue
+
+      if (raw_block and l != "Parsed result:"):
+        self.raw += l + "\n"
+        continue
+
+      if (raw_block and l == "Parsed result:"):
+        raw_block = False
+        parsed_block = True
+        continue
+
+      if (parsed_block and not re.match("Also, there were", l)):
+        self.data += l + "\n" 
+        continue
+    
+      if (parsed_block and re.match("Also, there were", l)):
+        parsed_block = False
+        point_block = True
+        continue
+      
+      if (point_block):
+        m = re.search("Point\s(\d+):\s\(([\d\.]+),([\d\.]+)\)", l)
+        if (m):
+          print m.group(1) + m.group(2) + m.group(3)
+          self.points.append((float(m.group(2)), float(m.group(3))))
+        
+    return
